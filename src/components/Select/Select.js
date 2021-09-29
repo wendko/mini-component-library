a1c3335e0a79b2e227990d5df816304619e5e961
wendko <wendko.88@gmail.com>
2021-09-30 02:10:50 +0800

    feat: select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,46 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const getWidth = (value) => {
+    const width = value.length + 'rem';
+    return width;
+}
+
+const SelectElement = styled.select`
+    background: ${COLORS.gray50};
+    border-radius: 8px;
+    width: min-content;
+    max-width: ${props => getWidth(props.displayedValue)};
+
+    border: 12px solid ${COLORS.gray50};
+    color: ${COLORS.gray700};
+    
+    &:hover {
+        color: ${COLORS.black};
+    }
+
+    &:after {
+        content: ${props => props.value};
+        position: absolute;
+    }
+`;
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectElement value={value} displayedValue={displayedValue} onChange={onChange}>
+        {children.map(child => {
+            if (child.props.value === value) {
+                return <option selected value={child.props.value}>{child.props.children}</option>;
+            }
+            return <option value={child.props.value}>{child.props.children}</option>;
+        })}
+    </SelectElement>
   );
 };
 
 export default Select;
+ 
